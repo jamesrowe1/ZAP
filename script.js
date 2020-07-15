2ab8cb1d46a04e0f9dff2bdc7e6074ee02fd0ff6
James Rowe <jamesrowe@outlook.com>
2020-07-15 18:43:15 -0400

    pseudo code

--- script.js
+++ script.js
@@ -8,44 +8,18 @@ $(document).ready(function () {
 //User Nav
 // if user click on the logo - button
 // button returns user to main page
-// drop down menu (hamurger icon
-//     1.search games button with space for user input or directs to user input page
-//     2.about ZAP button that links to page with images and info about ZAP App
+//searchbar
 
-// Possible Search categories
-//     1. game prices
-//     2. ESRB rating (Entertainment Software Rating Board: E= everyone; E10+= Everyone 10 and older; T=Teen; M=Mature 17+; A=Adults only; RP - Rating Pending
-//     3. popularity
-
-// The drop down menu - buttons
-//     Each button pulls the correspodning data for the game
-//onclick funciton
-//link to first html
-//3 separate carousel containers of different games from API
-//buttons of each image if not cards
-//each image is a button
-//below the image there are buttons (like, info, ratings) (click then reveal price)
+//cards of image from game plus info
 //pull categories of data
-//drop down menu categories with categories that link with buttons
-//each button
-//contact page
-//about page linked to (link and logo)
-
-//  About Zap Page
-//     Image
-//     paragraph explaining the App- asscess to thousands of gaes to quickly search...
-
-// Game Search Page
-//     User input for game name -
-//     User input for genre
-//     Uer input for age
+//save button
+//share button
 
 //     button
 //     on click
 //     pull from API
 //     pulls up lists according to User input
 
-//     and then theuser needs to click on???
 //get specific details from rawg api
 
 $.ajax({
