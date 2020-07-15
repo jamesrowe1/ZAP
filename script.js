be386ed82d5e187fd0110a03813d13f5690412c1
James Rowe <jamesrowe@outlook.com>
2020-07-15 16:00:07 -0400

    game info

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 $(document).ready(function () {
   $(".sidenav").sidenav();
 });
+
+$.ajax({
+  url: "https://api.rawg.io/api/games/fortnite",
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+});
