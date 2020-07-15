81e4882d8e2176857f2344be8ae97c5411358319
Melissa Mercado <melissa.a.merc@gmail.com>
2020-07-15 15:40:49 -0400

    psuedo code

--- script.js
+++ script.js
@@ -3,3 +3,22 @@
 $(document).ready(function () {
   $(".sidenav").sidenav();
 });
+
+//user enters page
+
+//zap logo is a button that brings you to splash/homepage page
+//onclick funciton
+//link to first html
+
+//3 separate carosul containers of different games from API
+//buttons of each image if not cards
+//each image is a button
+
+//below the image there are buttons (like, info, ratings) (click then reveal price)
+//pull categories of data
+//drop down menu categories with categories that link with buttons
+//each button
+
+//contact page
+
+//about page linked to (link and logo)
