2d7b22ae0d24a3c9f889a953e60a15af7dc16a10
James Rowe <jamesrowe@outlook.com>
2020-07-16 10:38:57 -0400

    created global vars

--- script.js
+++ script.js
@@ -42,7 +42,12 @@ function shareGame() {
 //     pulls up lists according to User input
 
 //get specific details from rawg api
-
+var gameName = "World of Warcraft";
+var gameRawg = game.replace(/\s+/g, "-").toLowerCase();
+var gameCheap = game.replace(/\s/g);
+var gameDescription = "we are awesome coders";
+var gameImageUrl =
+  "https://media.rawg.io/media/games/1dc/1dca31934274ae06195b71cafe56f375.jpg";
 $.ajax({
   url: "https://api.rawg.io/api/games/world-of-warcraft",
   method: "GET",
