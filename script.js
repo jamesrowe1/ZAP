6336bcd016a59f0b8e93ccbfaf75f98ad739c49b
James Rowe <jamesrowe@outlook.com>
2020-07-16 13:51:02 -0400

    search finally works

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // jQuery
+//DOM Elements
+var searchBar = $("#search");
 
 $(document).ready(function () {
   $(".sidenav").sidenav();
@@ -83,14 +85,7 @@ function shareGame() {
 
 //get specific details from rawg api
 var gameName = "lego Batman";
-var esrb;
-// p.text(results[i].rating);
 
-//Rawg API has weird naming conventions
-var gameRawg = gameName.replace(/[^a-zA-Z0-9]/g, "-").toLowerCase();
-gameRawg = gameRawg.replace(/-{2,}/g, "-");
-//CheapShark API also has weird naming conventions
-var gameCheap = gameName.replace(/[^a-zA-Z0-9]/g, "").toUpperCase();
 var price = "$19.99";
 var storeCheapID;
 var gameDescription = "we are awesome coders";
@@ -98,71 +93,48 @@ var gameImageUrl;
 var gameCheapID;
 var esrb = "";
 
-//gets details from rawgAPI
-$.ajax({
-  url: "https://api.rawg.io/api/games/" + gameRawg,
-  method: "GET",
-}).then(function (rawgResponse) {
-  console.log(rawgResponse);
-  //esrb rating
-  if (rawgResponse.esrb_rating === null) {
-    esrb = "No ESRB Rating";
-  } else {
-    esrb = rawgResponse.esrb_rating.name;
-  }
-  //this has <p> in it. let's try to use it to make the card clean
-  gameDescription = rawgResponse.description;
-  //setting the image
-  gameImageUrl = rawgResponse.background_image;
-  console.log(esrb);
-  console.log(gameDescription);
-  console.log(gameImageUrl);
-});
+searchBar.on("keypress", function (event) {
+  var keycode = event.keyCode;
+  if (keycode === 13) {
+    event.preventDefault();
+    gameName = searchBar.val();
+    console.log(gameName);
+    //all the ajax stuff here
+    //Rawg API has weird naming conventions
+    var gameRawg = gameName.replace(/[^a-zA-Z0-9]/g, "-").toLowerCase();
+    gameRawg = gameRawg.replace(/-{2,}/g, "-");
+    //CheapShark API also has weird naming conventions
+    var gameCheap = gameName.replace(/[^a-zA-Z0-9]/g, "").toUpperCase();
 
-//get array of games closely matching from cheapshark api
-var settings = {
-  async: true,
-  crossDomain: true,
-  url:
-    "https://cheapshark-game-deals.p.rapidapi.com/games?limit=60&title=" +
-    gameCheap +
-    "&exact=0",
-  method: "GET",
-  headers: {
-    "x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
-    "x-rapidapi-key": "629a103ae7msh8d2e000534865ffp18dc6ejsna10a77d719b1",
-  },
-};
+    //gets details from rawgAPI
+    $.ajax({
+      url: "https://api.rawg.io/api/games/" + gameRawg,
+      method: "GET",
+    }).then(function (rawgResponse) {
+      console.log(rawgResponse);
+      //esrb rating
+      if (rawgResponse.esrb_rating === null) {
+        esrb = "No ESRB Rating";
+      } else {
+        esrb = rawgResponse.esrb_rating.name;
+      }
+      //this has <p> in it. let's try to use it to make the card clean
+      gameDescription = rawgResponse.description;
+      //setting the image
+      gameImageUrl = rawgResponse.background_image;
+      console.log(esrb);
+      console.log(gameDescription);
+      console.log(gameImageUrl);
+    });
 
-$.ajax(settings).done(function (cheapResponseArr) {
-  console.log(cheapResponseArr);
-  gameCheapID = cheapResponseArr[0].gameID;
-
-  //nested call to cheap as details are needed from above
-  //get specific info about 1 game (wow) cheapshark
-  var settings = {
-    async: true,
-    crossDomain: true,
-    url: "https://cheapshark-game-deals.p.rapidapi.com/games?id=" + gameCheapID,
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
-      "x-rapidapi-key": "629a103ae7msh8d2e000534865ffp18dc6ejsna10a77d719b1",
-    },
-  };
-
-  $.ajax(settings).done(function (cheapResponseSingle) {
-    console.log(cheapResponseSingle);
-    price = cheapResponseSingle.deals[0].price;
-    console.log(price);
-    storeCheapID = cheapResponseSingle.deals[0].storeID;
-
-    //nested call to cheap as storeid needed from above
-    //cheapshark stores
+    //get array of games closely matching from cheapshark api
     var settings = {
       async: true,
       crossDomain: true,
-      url: "https://cheapshark-game-deals.p.rapidapi.com/stores",
+      url:
+        "https://cheapshark-game-deals.p.rapidapi.com/games?limit=60&title=" +
+        gameCheap +
+        "&exact=0",
       method: "GET",
       headers: {
         "x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
@@ -170,11 +142,53 @@ $.ajax(settings).done(function (cheapResponseArr) {
       },
     };
 
-    $.ajax(settings).done(function (cheapStoresResponse) {
-      console.log(storeCheapID);
-      console.log(cheapStoresResponse);
-      //cause store id is +1 of array value...
-      console.log(cheapStoresResponse[storeCheapID - 1].storeName);
+    $.ajax(settings).done(function (cheapResponseArr) {
+      console.log(cheapResponseArr);
+      gameCheapID = cheapResponseArr[0].gameID;
+
+      //nested call to cheap as details are needed from above
+      //get specific info about 1 game (wow) cheapshark
+      var settings = {
+        async: true,
+        crossDomain: true,
+        url:
+          "https://cheapshark-game-deals.p.rapidapi.com/games?id=" +
+          gameCheapID,
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
+          "x-rapidapi-key":
+            "629a103ae7msh8d2e000534865ffp18dc6ejsna10a77d719b1",
+        },
+      };
+
+      $.ajax(settings).done(function (cheapResponseSingle) {
+        console.log(cheapResponseSingle);
+        price = cheapResponseSingle.deals[0].price;
+        console.log(price);
+        storeCheapID = cheapResponseSingle.deals[0].storeID;
+
+        //nested call to cheap as storeid needed from above
+        //cheapshark stores
+        var settings = {
+          async: true,
+          crossDomain: true,
+          url: "https://cheapshark-game-deals.p.rapidapi.com/stores",
+          method: "GET",
+          headers: {
+            "x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
+            "x-rapidapi-key":
+              "629a103ae7msh8d2e000534865ffp18dc6ejsna10a77d719b1",
+          },
+        };
+
+        $.ajax(settings).done(function (cheapStoresResponse) {
+          console.log(storeCheapID);
+          console.log(cheapStoresResponse);
+          //cause store id is +1 of array value...
+          console.log(cheapStoresResponse[storeCheapID - 1].storeName);
+        });
+      });
     });
-  });
+  }
 });
