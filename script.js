a175905766d0cbf5737adc0a13d805375e24be3c
T Lom <tiffanyplombardi@gmail.com>
2020-07-17 18:45:41 -0400

    new modal

--- script.js
+++ script.js
@@ -280,67 +280,6 @@ function addCard(game) {
   cardButtons.append(likeButton);
   cardDiv.append(cardButtons);
   bigContainer.prepend(cardDiv);
-
-  $(".btn-share").on("click", function (event) {
-    //   // get most recent submission
-    //   //unsure of gametitle
-    alert("It works");
-    //   //   remember to tak out the alert!!!
-    // make a modal with a linkto the image
-
-    $(window).on("load", function () {
-      $.expr[":"].external = function (a) {
-        var linkhn = a.hostname.split(".").reverse();
-        var linkHref = linkhn[1] + "." + linkhn[0];
-
-        var domainhn = window.location.hostname.split(".").reverse();
-        var domainHref = domainhn[1] + "." + domainhn[0];
-
-        return (
-          !a.href.match(/^mailto\:/) &&
-          !a.href.match(/^tel\:/) &&
-          linkHref !== domainHref
-        );
-      };
-
-      $("a:external").addClass("ext_link");
-
-      $(function () {
-        $("a.ext_link").click(function () {
-          // open a modal
-          $("a:external").attr("data-toggle", "modal");
-          $("a:external").attr("data-target", "#speedbump");
-          //go to link on modal close
-          var url = $(this).attr("href");
-          $(".btn-modal.btn-continue").click(function () {
-            window.open(url);
-            $(".btn-modal.btn-continue").off();
-          });
-          $(".btn-modal.btn-close").click(function () {
-            $("#speedbump").modal("hide");
-            $(".btn-modal.btn-close").off();
-          });
-        });
-      });
-    });
-    //   //unsure of gametitle
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-    modal.style.display = "block";
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-      modal.style.display = "none";
-    };
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-      if (event.target !== modal) {
-        modal.style.display = "none";
-      }
-    };
-  });
 }
 
 function likeBtnClick(event) {
