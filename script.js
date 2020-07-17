46c5df3530a9737e456ba2fcd7518ea2c65b8506
T Lom <tiffanyplombardi@gmail.com>
2020-07-17 16:42:48 -0400

    Here I am trying to re add all my javascript code for the modal including hide attempts basically everything that I copied to a different file as a precaution due to git problems

--- script.js
+++ script.js
@@ -257,10 +257,33 @@ function addCard() {
     //localStorage.setItem("user", JSON.stringify(user));
   });
   // share button here
-  $(".btn-share").on("click", function (event) {
+  $('.btn-share').on('click', function (event) {
+    $("#hide").click(function() {
+    }
     //   // get most recent submission
     //   //unsure of gametitle
-    alert("It works");
+    alert('It works')
     //   //   remember to tak out the alert!!!
+    // make a modal with a linkto the image
+
+    // Get the modal
+    var modal = document.getElementById('myModal')
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName('close')[0]
+    modal.style.display = 'block'
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = 'none'
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target !== modal) {
+        modal.style.display = 'none'
+      }
+    }
   });
+
 }
